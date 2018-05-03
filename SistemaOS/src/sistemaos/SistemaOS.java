fec3a7a9e5bd096501dfe3e77f6c20eb07b7b320
Vilanrel <37760807+Vilanrel@users.noreply.github.com>
2018-05-02 21:57:28 -0300

    Alterando o Código

--- SistemaOS/src/sistemaos/SistemaOS.java
+++ SistemaOS/src/sistemaos/SistemaOS.java
@@ -6,7 +6,7 @@ public class SistemaOS {
 
     public static void main(String[] args) {
         
-        String showInputDialog = JOptionPane.showInputDialog("Vanilton Breda");
+        JOptionPane.showMessageDialog("Vanilton Breda dos Santos");
     }
     
 }
