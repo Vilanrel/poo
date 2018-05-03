00e8ee19382fd04a6dca5d5cf17964bfcbd52008
Vilanrel <37760807+Vilanrel@users.noreply.github.com>
2018-05-03 12:01:05 -0300

    Update SistemaOS.java

--- SistemaOS/src/sistemaos/SistemaOS.java
+++ SistemaOS/src/sistemaos/SistemaOS.java
@@ -1,11 +1,9 @@
 package sistemaos;
-
 import javax.swing.JOptionPane;
 
 public class SistemaOS {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {       
         JOptionPane.showMessageDialog("Vanilton Breda dos Santos");
     }
     
